435d5812922e9ea931ce65e75bc27f3d2344016d
Travis Brown <travisdanielbrown@proton.me>
2025-01-23 14:35:22 -0500

    init commit

---
+++ Travis_Brown_Scheduling_Application/Login.cs
@@ -0,0 +1,10 @@
+namespace Travis_Brown_Scheduling_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
