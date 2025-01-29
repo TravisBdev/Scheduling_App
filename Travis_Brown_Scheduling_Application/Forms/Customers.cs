80c362c3b1022407c51f36860c605628a9286438
Travis Brown <travisdanielbrown@proton.me>
2025-01-29 16:46:28 -0500

    Created Customers form and initial design.

---
+++ Travis_Brown_Scheduling_Application/Forms/Customers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Travis_Brown_Scheduling_Application.Forms {
+    public partial class Customers : Form {
+        public Customers() {
+            InitializeComponent();
+        }
+    }
+}
