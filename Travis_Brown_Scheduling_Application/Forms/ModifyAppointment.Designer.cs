48917ac4251c2c5c803e2a7340661508059eba67
Travis Brown <travisdanielbrown@proton.me>
2025-01-31 08:57:07 -0500

    Changing design of ModifyAppointment page

--- Travis_Brown_Scheduling_Application/Forms/ModifyAppointment.Designer.cs
+++ Travis_Brown_Scheduling_Application/Forms/ModifyAppointment.Designer.cs
@@ -23,10 +23,16 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ModifyAppointment";
+            SuspendLayout();
+            // 
+            // ModifyAppointment
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "ModifyAppointment";
+            Text = "Modify Appointment";
+            ResumeLayout(false);
         }
 
         #endregion
