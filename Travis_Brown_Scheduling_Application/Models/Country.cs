48853ddc3aa1f87755142374a081d4064c6f685e
Travis Brown <travisdanielbrown@proton.me>
2025-01-31 13:01:10 -0500

    Created Country model - Added default values for audit properties.

--- Travis_Brown_Scheduling_Application/Models/Country.cs
+++ Travis_Brown_Scheduling_Application/Models/Country.cs
@@ -5,10 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Travis_Brown_Scheduling_Application.Models {
-    public class City {
-        public int CityId { get; set; }
-        public string CityName { get; set; }
+    public class Country {
         public int CountryId { get; set; }
+        public string CountryName { get; set; }
         public DateTime CreateDate { get; set; } = DateTime.UtcNow;
         public string CreatedBy { get; set; } = "System";
         public DateTime LastUpdate { get; set; } = DateTime.UtcNow;
